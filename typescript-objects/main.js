375c6b125ad754882fe59be5c7ef46752d55af4a
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-10 22:06:18 +0000

    complete typescript-object

--- typescript-objects/main.js
+++ typescript-objects/main.js
@@ -4,5 +4,32 @@ const student = {
   lastName: 'Skywalker',
   age: 35,
 };
+const pet = {
+  name: 'fluffy',
+  kind: 'British',
+};
+const vehicle = {
+  make: 'SoroSuub',
+  model: 'Landspeeder',
+  year: Number('20BBY'),
+};
 const fullName = `${student.firstName} ${student.lastName}`;
-console.log(`value of fullName ${fullName}`);
+console.log(`value of fullName: ${fullName}`);
+student.livesInIrvine = false;
+console.log(`value of student.livesInIrvine: ${student.livesInIrvine}`);
+student.previousOccupation = 'student';
+console.log(
+  `value of student.previousOccupation: ${student.previousOccupation}`
+);
+console.log('value of student:', student);
+console.log('typeof student:', typeof student);
+vehicle['color'] = 'White';
+vehicle['isConvertible'] = true;
+console.log(`value of vehicle["color"]: ${vehicle['color']}`);
+console.log(`value of vehicle["isConvertible"]: ${vehicle['isConvertible']}`);
+console.log('value of vehicle:', vehicle);
+console.log('typeof vehicle:', typeof vehicle);
+delete pet.name;
+delete pet.kind;
+console.log('value of pet', pet);
+console.log('typeof pet', typeof pet);
--- typescript-objects/main.ts
+++ typescript-objects/main.ts
@@ -12,5 +12,55 @@ const student: Studentprops = {
   age: 35,
 };
 
+interface Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  color?: string;
+  isConvertible?: boolean;
+}
+
+interface Pet {
+  name?: string;
+  kind?: string;
+}
+
+const pet: Pet = {
+  name: 'fluffy',
+  kind: 'British',
+};
+
+const vehicle: Vehicle = {
+  make: 'SoroSuub',
+  model: 'Landspeeder',
+  year: Number('20BBY'),
+};
+
 const fullName: string = `${student.firstName} ${student.lastName}`;
-console.log(`value of fullName ${fullName}`);
+console.log(`value of fullName: ${fullName}`);
+
+student.livesInIrvine = false;
+
+console.log(`value of student.livesInIrvine: ${student.livesInIrvine}`);
+
+student.previousOccupation = 'student';
+
+console.log(
+  `value of student.previousOccupation: ${student.previousOccupation}`
+);
+console.log('value of student:', student);
+console.log('typeof student:', typeof student);
+
+vehicle['color'] = 'White';
+vehicle['isConvertible'] = true;
+
+console.log(`value of vehicle["color"]: ${vehicle['color']}`);
+console.log(`value of vehicle["isConvertible"]: ${vehicle['isConvertible']}`);
+console.log('value of vehicle:', vehicle);
+console.log('typeof vehicle:', typeof vehicle);
+
+delete pet.name;
+delete pet.kind;
+
+console.log('value of pet', pet);
+console.log('typeof pet', typeof pet);

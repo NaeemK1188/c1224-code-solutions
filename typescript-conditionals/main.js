d67b7cc276b21b12aa769e7d98bdea0cced37772
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-13 11:22:48 +0000

    complete typescript-conditionals

--- typescript-conditionals/main.js
+++ typescript-conditionals/main.js
@@ -1,40 +1,20 @@
-/* exported isUnderFive,
-            isEven,
-            startsWithJ,
-            isOldEnoughToDrink,
-            isOldEnoughToDrive,
-            isOldEnoughToDrinkAndDrive,
-            categorizeAcidity,
-            introduceWarnerBro,
-            recommendMovie
- */
-// ==============================IsUnderFive()============================================
-interface Person {
-  // defining object
-  name: string;
-  age: number;
-}
-
-function isUnderFive(age: number): boolean {
+'use strict';
+function isUnderFive(age) {
   if (age < 5) {
     return true;
   }
-
   // if person.age == 5, person.age > 5
   else {
     return false;
   }
 }
-
 console.log('isUnderFive Function()');
 console.log(isUnderFive(4));
 console.log(isUnderFive(10));
 console.log(isUnderFive(5));
-
 // =========================================================================================
 // ======================isEven()=============================================================
-
-function isEven(num: number): boolean {
+function isEven(num) {
   if (num % 2 === 0) {
     // 4/2 = 2 remainder 0
     return true;
@@ -43,27 +23,22 @@ function isEven(num: number): boolean {
     return false;
   }
 }
-
 console.log('isEven Function()');
 console.log(isEven(4));
 console.log(isEven(10));
 console.log(isEven(5));
-
 // ==============================================================================
 // ==================================startsWithJ()===============================
 // string is an array
 // const stringnam = "noah";
-
 // console.log(stringnam[stringnam.length - 1]);
-
-function startsWithJ(name: string): boolean {
+function startsWithJ(name) {
   if (name[0] === 'J') {
     return true;
   } else {
     return false;
   }
 }
-
 console.log('startsWithJ()');
 console.log(startsWithJ('Javascript'));
 console.log(startsWithJ('PHP'));
@@ -71,52 +46,42 @@ console.log(startsWithJ('HTML'));
 console.log(startsWithJ('JAVA'));
 console.log(startsWithJ('KOTLIN'));
 console.log(startsWithJ('C#'));
-
 // ==============================================================================
 // ===========================isOldEnoughToDrink()=================================
-
-function isOldEnoughToDrink(person: Person): boolean {
+function isOldEnoughToDrink(person) {
   if (person.age < 21) {
     return false;
   } else {
     return true;
   }
 }
-
 console.log('isOldEnoughToDrink()');
 console.log(isOldEnoughToDrink({ name: 'Bart William', age: 10 }));
 // ============================================================================
 // ========================isOldEnoughToDrive()=================================
-
-function isOldEnoughToDrive(person: Person): boolean {
+function isOldEnoughToDrive(person) {
   if (person.age < 16) {
     return false;
   } else {
     return true;
   }
 }
-
 console.log('isOldEnoughToDrive()');
 console.log(isOldEnoughToDrive({ name: 'William McCarthy', age: 39 }));
-
 // =============================================================================
 // ==========================isOldEnoughToDrinkAndDrive()=======================
-
-function isOldEnoughToDrinkAndDrive(person: Person): boolean {
+function isOldEnoughToDrinkAndDrive(person) {
   if (person.name) {
     return false;
   } else {
     return false;
   }
 }
-
 console.log('isOldEnoughToDrinkAndDrive()');
 console.log(isOldEnoughToDrinkAndDrive({ name: 'William McCarthy', age: 39 }));
-
 // ==================================================================================
 // =============================categorizeAcidity()=================================
-
-function isCategorizeAcidity(pH: number): string {
+function isCategorizeAcidity(pH) {
   if (pH === 7) {
     return 'pH is neutral';
   } else if (pH >= 0 && pH < 7) {
@@ -127,7 +92,6 @@ function isCategorizeAcidity(pH: number): string {
     return 'invalid pH level';
   }
 }
-
 console.log('isCategorizeAcidity()');
 console.log(isCategorizeAcidity(-1));
 console.log(isCategorizeAcidity(14.000001));
@@ -136,8 +100,7 @@ console.log(isCategorizeAcidity(2));
 console.log(isCategorizeAcidity(9));
 // ===================================================================================
 // ================================introduceWarnerBro()===============================
-
-function introduceWarnerBro(name: string): string {
+function introduceWarnerBro(name) {
   switch (name) {
     case 'yakko':
       return 'We are the warner brothers!';
@@ -149,7 +112,6 @@ function introduceWarnerBro(name: string): string {
       return 'Goodnight everybody!';
   }
 }
-
 console.log('introduceWarnerBro()');
 console.log(introduceWarnerBro('dot'));
 console.log(introduceWarnerBro('wakko'));
@@ -157,8 +119,7 @@ console.log(introduceWarnerBro('yakko'));
 console.log(introduceWarnerBro('cody'));
 // ===============================================================================
 // =======================recommendMovie()=======================================
-
-function recommendMovie(genre: string): string {
+function recommendMovie(genre) {
   switch (genre) {
     case 'action':
       return 'Batman';
@@ -176,7 +137,6 @@ function recommendMovie(genre: string): string {
       return 'enter the right genre';
   }
 }
-
 console.log('recommendMovie()');
 console.log(recommendMovie('action'));
 console.log(recommendMovie('comedy'));

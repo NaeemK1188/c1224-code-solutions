26b1fd6f2de52a3229baeecbf642ead1b5340f37
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-13 06:07:49 +0000

    complete typescript-array

---
+++ typescript-arrays/javascript-arrays-quiz-notes.md
@@ -0,0 +1,57 @@
+# javascript-arrays-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What are arrays used for?
+
+  Holds collection of different data types
+
+- Describe array literal notation.
+
+  Open bracket with commas separating arrays elements, then close bracket
+
+- How are arrays different from "plain" objects?
+
+  Ordering elements, which have a specified order and can be accessed by indexes.
+
+- What number represents the first index of an array?
+
+  zero
+
+- What is the `length` property of an array?
+
+  Measures how many items in the array
+
+- How do you calculate the last index of an array?
+
+  array.length - 1
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ typescript-arrays/main.js
@@ -0,0 +1,20 @@
+'use strict';
+// ========================COLOR ARRAY============================================================
+const colors = ['red', 'white', 'blue'];
+console.log('value of color[0]:', colors[0]);
+console.log('value of color[1]:', colors[1]);
+console.log('value of color[2]:', colors[2]);
+console.log(`America is ${colors[0]}, ${colors[1]}, and ${colors[2]}`); // template literal (``)
+colors[2] = 'green';
+console.log(`America is ${colors[0]}, ${colors[1]}, and ${colors[2]}`);
+console.log('value of colors:', colors);
+console.log("the type of colors' arrays:", typeof colors);
+// ====================================================================================================
+// ================================STUDENT ARRAY=======================================================
+const students = ['Leonardo', 'Donatello', 'Rafael', 'Michelangelo'];
+const numberOfStudents = students.length;
+console.log(`There are ${numberOfStudents} students in the class`);
+const lastIndex = numberOfStudents - 1; // students[students.length - 1]
+console.log('the last student in the array is', students[lastIndex]);
+console.log('value of students', students);
+console.log("the type of students' array:", typeof students);

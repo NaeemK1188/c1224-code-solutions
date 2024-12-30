60300a9e1af4b7607d2ecb13df74deefc17e0585
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-30 10:18:55 +0000

    get-indexes

--- typescript-loops-practice/get-indexes.ts
+++ typescript-loops-practice/get-indexes.ts
@@ -1,1 +1,12 @@
 /* exported getIndexes */
+function getIndexes(arrAny: any[]): number[] {
+  const arr: any[] = [];
+  for (let i = 0; i < arrAny.length; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+const input2: any[] = getIndexes(['meow', 'woof', 'squeak', 'shazoo']);
+console.log(input2);

72014207d2a0e26b09430f03c57a0c65c353c21c
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-31 00:29:45 +0000

    filter-out-nulls

--- typescript-loops-practice/filter-out-nulls.js
+++ typescript-loops-practice/filter-out-nulls.js
@@ -1,2 +1,16 @@
 'use strict';
 /* exported filterOutNulls */
+function filterOutNulls(arrAny) {
+  const newArr = [];
+  for (let i = 0; i < arrAny.length; i++) {
+    if (arrAny[i] == null) {
+      // false never executed
+      continue;
+    } else {
+      newArr.push(arrAny[i]);
+    }
+  }
+  return newArr;
+}
+const input7 = filterOutNulls(['a', null, 'b', null]);
+console.log(input7);
--- typescript-loops-practice/filter-out-nulls.ts
+++ typescript-loops-practice/filter-out-nulls.ts
@@ -1,1 +1,18 @@
 /* exported filterOutNulls */
+
+function filterOutNulls(arrAny: any[]): any[] {
+  const newArr: any[] = [];
+  for (let i = 0; i < arrAny.length; i++) {
+    if (arrAny[i] == null) {
+      // false never executed
+      continue;
+    } else {
+      newArr.push(arrAny[i]);
+    }
+  }
+
+  return newArr;
+}
+
+const input7: any[] = filterOutNulls(['a', null, 'b', null]);
+console.log(input7);

2978da7e242d691215a7ff50654abb269baeb93f
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-31 02:15:58 +0000

    include-seven

--- typescript-loops-practice/includes-seven.ts
+++ typescript-loops-practice/includes-seven.ts
@@ -1,1 +1,14 @@
 /* exported includesSeven */
+
+function includesSeven(arrAny: any[]): boolean {
+  for (let i = 0; i < arrAny.length; i++) {
+    if (arrAny[i] === 7) {
+      return true;
+    }
+  }
+
+  return false; // return false because it didn't find any value of 7 like else
+}
+
+const input9: boolean = includesSeven([1, 2, 3, 4, 5, 6, 7]);
+console.log(input9);

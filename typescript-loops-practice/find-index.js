9d150b3e224f77e331dcaba13d643dbcf38fab8e
NaeemK1188 <naeemalbirkdar@aol.com>
2024-12-31 02:38:49 +0000

    find-index

--- typescript-loops-practice/find-index.js
+++ typescript-loops-practice/find-index.js
@@ -1,2 +1,12 @@
 'use strict';
 /* exported findIndex */
+function findIndex(arrNum, numVal) {
+  for (let i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] === numVal) {
+      return i;
+    }
+  }
+  return -1; // else return -1, the compiler knows that this is else option, so no need to write else
+}
+const input11 = findIndex([1, 5, 3], 5);
+console.log(input11);
--- typescript-loops-practice/find-index.ts
+++ typescript-loops-practice/find-index.ts
@@ -1,1 +1,14 @@
 /* exported findIndex */
+
+function findIndex(arrNum: number[], numVal: number): number {
+  for (let i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] === numVal) {
+      return i;
+    }
+  }
+
+  return -1; // else return -1, the compiler knows that this is else option, so no need to write else
+}
+
+const input11: number = findIndex([1, 5, 3], 5);
+console.log(input11);

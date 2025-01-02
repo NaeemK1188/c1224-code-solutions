acea35a95f5c8d550ded10d9e6ee27ec45bf4dfc
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-02 07:10:18 +0000

    initial()

--- array-manipulation-1/src/tail.ts
+++ array-manipulation-1/src/tail.ts
@@ -1,1 +1,13 @@
 /* exported tail */
+
+function tail(arrAny: unknown[]): unknown[] {
+  const newArr: unknown[] = [];
+  for (let i = 1; i < arrAny.length; i++) {
+    newArr.push(arrAny[i]);
+  }
+
+  return newArr;
+}
+
+const input2: unknown = tail(['foo', 'bar', 'baz']);
+console.log(input2);

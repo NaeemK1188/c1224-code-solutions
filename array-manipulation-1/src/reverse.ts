f35e017ed61a31de2a118862578a9f05f03b56ab
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-02 07:25:34 +0000

    reverse()

--- array-manipulation-1/src/reverse.ts
+++ array-manipulation-1/src/reverse.ts
@@ -1,1 +1,13 @@
 /* exported reverse */
+
+function reverse(arrAny: unknown[]): unknown[] {
+  const newArr: unknown[] = [];
+  for (let i = arrAny.length - 1; i >= 0; i--) {
+    newArr.push(arrAny[i]);
+  }
+
+  return newArr;
+}
+
+const input4: unknown = reverse(['foo', 'bar', 'baz']);
+console.log(input4);

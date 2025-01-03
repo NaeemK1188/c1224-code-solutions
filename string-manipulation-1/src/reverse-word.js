60a4a50c5e49339066a76b5fd028c323b7decd2b
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-03 08:09:59 +0000

    complete string-manipulation-1

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,2 +1,19 @@
 'use strict';
 /* exported reverseWord */
+function reverseWord(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr = newStr + str[i];
+  }
+  return newStr;
+}
+let input7 = reverseWord('foo');
+console.log(input7);
+input7 = reverseWord('rab');
+console.log(input7);
+input7 = reverseWord('LearningFuze');
+console.log(input7);
+input7 = reverseWord('tpircSavaJ');
+console.log(input7);
+input7 = reverseWord('racecar');
+console.log(input7);

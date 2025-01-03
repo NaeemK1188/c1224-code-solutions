58b7afc022b09655cec8c366f500fd4d6d4545f6
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-03 06:18:09 +0000

    isLowerCased()

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,2 +1,21 @@
 'use strict';
 /* exported isUpperCased */
+function isUpperCased(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true; // after checking all characters in the string, it returns true
+  //, which no character was found lowercased
+}
+let input3 = isUpperCased('LearningFuze');
+console.log(input3);
+input3 = isUpperCased('JavaScript');
+console.log(input3);
+input3 = isUpperCased('HTML');
+console.log(input3);
+input3 = isUpperCased('css');
+console.log(input3);
+input3 = isUpperCased('PHP');
+console.log(input3);
--- string-manipulation-1/src/is-upper-cased.ts
+++ string-manipulation-1/src/is-upper-cased.ts
@@ -1,1 +1,27 @@
 /* exported isUpperCased */
+
+function isUpperCased(str: string): boolean {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      return false;
+    }
+  }
+
+  return true; // after checking all characters in the string, it returns true
+  //, which no character was found lowercased
+}
+
+let input3: boolean = isUpperCased('LearningFuze');
+console.log(input3);
+
+input3 = isUpperCased('JavaScript');
+console.log(input3);
+
+input3 = isUpperCased('HTML');
+console.log(input3);
+
+input3 = isUpperCased('css');
+console.log(input3);
+
+input3 = isUpperCased('PHP');
+console.log(input3);

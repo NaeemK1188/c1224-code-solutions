dcad1de5a69709b3554b75ae11ca0ca07691f681
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-04 12:09:12 +0000

    complete object-manipulation-1

--- object-manipulation-1/src/to-object.ts
+++ object-manipulation-1/src/to-object.ts
@@ -1,1 +1,18 @@
 /* exported toObject */
+
+interface Obj4 {
+  firstName?: string;
+  isCool?: boolean;
+  employer?: string;
+}
+
+function toObject(objArr: any[]): Obj4 {
+  const obj4: Obj4 = {};
+
+  obj4[objArr[0]] = objArr[1];
+
+  return obj4;
+}
+
+const input4: Obj4 = toObject(['firstName', 'David']);
+console.log(input4);

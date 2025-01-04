bd6fc516f28603658a1fb1db1913bbdc8eef56b3
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-04 09:25:14 +0000

    setValue()

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,2 +1,10 @@
 'use strict';
 /* exported setValue */
+const obj1 = {
+  firstName: 'Dave',
+};
+function setValue(obj1, key, value) {
+  obj1[key] = value;
+}
+setValue(obj1, 'lastName', 'Thomas');
+console.log(obj1);

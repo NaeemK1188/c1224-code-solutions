cf35ea914adfe05ae330697a2d4f9c46a0f95ef8
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-09 21:39:25 +0000

    working on main.ts

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+// let currentIndex = 0;
+// if (event.key === span[$currentIndex].textContent) {
+//   currentIndex++
+// }
+// span[i].className = '';
+// span[i + 1].className = 'dark';
+let index = 0;
+const $spans = document.querySelectorAll('span');
+console.log($spans);
---
+++ typescript-typing-tutor/main.ts
@@ -0,0 +1,18 @@
+// let currentIndex = 0;
+
+// if (event.key === span[$currentIndex].textContent) {
+//   currentIndex++
+// }
+
+// span[i].className = '';
+// span[i + 1].className = 'dark';
+
+
+
+
+let index:number = 0;
+
+
+const $spans = document.querySelectorAll('span')
+
+console.log($spans);

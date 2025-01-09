63310a9b4ed6c223adcd753eb86abc28fa6d793d
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-09 03:25:24 +0000

    working on main.ts

--- typescript-light-bulb/main.js
+++ typescript-light-bulb/main.js
@@ -1,13 +1,11 @@
+'use strict';
 // alert("JS is working");
-// let counter:number = 0;
-
+let counter = 0;
 const $lightBulb = document.querySelector('.light-bulb');
 console.log($lightBulb);
-
 if (!$lightBulb) {
   throw new Error('$lightBulb not exist');
 }
-
-$lightBulb.addEventListener('click', () => {
+$lightBulb.addEventListener('click', (event) => {
   console.log('light-bulb clicked');
 });

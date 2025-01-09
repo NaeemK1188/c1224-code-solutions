c2e7a39b2c294c3f059f43fe17b4f0dc60aba216
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-09 05:38:00 +0000

    complete typescript-light-bulb

--- typescript-light-bulb/main.ts
+++ typescript-light-bulb/main.ts
@@ -1,13 +1,31 @@
 // alert("JS is working");
-// let counter:number = 0;
+let counter: number = 0;
+let tempBulb: string = '';
+let tempContainer: string = '';
 
 const $lightBulb = document.querySelector('.light-bulb');
-console.log($lightBulb);
-
-if (!$lightBulb) {
-  throw new Error('$lightBulb not exist');
+// console.log($lightBulb); // test
+const $Container = document.querySelector('.container');
+// console.log($Container);
+if (!$lightBulb || !$Container) {
+  throw new Error('$lightBulb or $Container not exist');
 }
 
 $lightBulb.addEventListener('click', () => {
-  console.log('light-bulb clicked');
+  // console.log('light-bulb clicked'); // test
+  counter = counter + 1;
+  if (!$lightBulb || !$Container) {
+    throw new Error('$lightBulb or $Container query failed');
+  }
+
+  if (counter % 2 === 1) {
+    tempBulb = 'dark';
+    tempContainer = 'dark';
+  } else {
+    tempBulb = 'hot';
+    tempContainer = 'hot';
+  }
+
+  $lightBulb.className = 'light-bulb' + ' ' + tempBulb;
+  $Container.className = 'container' + ' ' + tempContainer;
 });

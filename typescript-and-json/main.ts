3d987e2a325d1ed67bd241507e7b48ca99ff49d4
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-13 18:03:06 +0000

    adding array of objects

---
+++ typescript-and-json/main.ts
@@ -0,0 +1,20 @@
+// interface Book
+// {
+
+// }
+
+// const books: string = [
+//   {
+//     isbn:"11223344",
+//     title: 'python crash course',
+//     author: 'make oscar'
+//   },
+//   {
+//     isbn:"55667788",
+//     title:"C language",
+//     auther:"Dinnes Rishi"
+//   },
+//   {
+
+//   }
+// ]

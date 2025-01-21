2dbcfcbbdb03483a92e2034be9241b453a5ff87c
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-21 18:21:32 +0000

    practice-interview-challenge

---
+++ practice-interview-challenge/main.ts
@@ -0,0 +1,53 @@
+interface Calc {
+  average: number;
+  evens: number[];
+  odds: number[];
+  range: number[];
+  total: number;
+}
+
+function getRangeReport(start: number, end: number): Calc {
+  const calc: Calc = {
+    average: 0,
+    evens: [],
+    odds: [],
+    range: [],
+    total: 0,
+  };
+
+  let s: number = start;
+  const e: number = end;
+
+  for (let i = s; i <= e; i++) {
+    calc.range.push(i);
+  }
+
+  s = 2;
+
+  for (let i = s; i <= e; i = i + 2) {
+    calc.evens.push(i);
+  }
+
+  s = 1;
+
+  for (let i = s; i <= e; i = i + 2) {
+    calc.odds.push(i);
+  }
+
+  s = 0;
+  calc.total = 0;
+  for (let i = s; i <= e; i++) {
+    calc.total = calc.total + i;
+  }
+  // for (let i = s; i <)
+
+  s = 0;
+
+  for (let i = s; i <= e; i++) {
+    calc.average = (calc.total + i) / 10;
+  }
+
+  return calc;
+}
+
+console.log(getRangeReport(1, 10));

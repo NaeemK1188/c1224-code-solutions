34e1979aa6669eb4272a14755c379c489657fa42
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-24 22:51:22 +0000

    complete typescript-promises

--- typescript-promises/main.ts
+++ typescript-promises/main.ts
@@ -1,2 +1,17 @@
 import { takeAChance } from './take-a-chance.js';
 // console.log(takeAChance('Hello world'));
+
+// console.log(takeAChance('Noah Khayat')) // output promise is pending because
+// we didn't yet handle the reject or resolve of the promise
+
+// assigning the promise object to variable
+const message = takeAChance('Noah Khayat');
+console.log(message);
+//  .then((message) => console.log(message)
+//   .catch((error) => console.error()
+
+message
+  .then((succeed) => console.log(succeed))
+  .catch((reject) => console.log(reject.message))
+  .finally(() => console.log('the operation is completed'));
+// or console.error(`Error: ${error.message}`))
--- typescript-promises/take-a-chance.ts
+++ typescript-promises/take-a-chance.ts
@@ -1,9 +1,13 @@
 export function takeAChance(name: string): Promise<string> {
+  // returning promise object
   return new Promise((resolve, reject) => {
+    // returning new object Promise with two parameters
     setTimeout(() => {
-      Math.random() <= 0.5
-        ? resolve(`Hooray! You're so lucky, ${name}!`)
+      // acting like a function
+      Math.random() <= 0.5 // condition if less than with random si it could be less or greater in order to get reject or resolve
+        ? // if promise fulfill                           :                 else promise reject
+          resolve(`Hooray! You're so lucky, ${name}!`)
         : reject(new Error(`It's just bad luck, ${name}.`));
-    }, 2000);
+    }, 2000); // delay 2 seconds
   });
 }

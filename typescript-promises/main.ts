d60d7c200bc9e910a9722491daa261c9b07b1c46
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-26 06:06:06 +0000

    update main.ts

--- typescript-promises/main.ts
+++ typescript-promises/main.ts
@@ -5,13 +5,26 @@ import { takeAChance } from './take-a-chance.js';
 // we didn't yet handle the reject or resolve of the promise
 
 // assigning the promise object to variable
+// assigning message will output the initial state pending
+// no resolve or reject
 const message = takeAChance('Noah Khayat');
 console.log(message);
 //  .then((message) => console.log(message)
 //   .catch((error) => console.error()
 
-message
-  .then((succeed) => console.log(succeed))
-  .catch((reject) => console.log(reject.message))
-  .finally(() => console.log('the operation is completed'));
+message // this for Promise((resolve,))
+  .then((resolve) => console.log(resolve)) // for resolve function
+  // this is for Promise((, reject))
+  .catch((reject) => console.log(reject.message)) // for reject function
+  .finally(() => console.log('the request is completed')); // output at the end of the request
 // or console.error(`Error: ${error.message}`))
+
+// or
+// message.then((resolve) => {
+//   // console.log("your so lucky");
+//   console.log(resolve);
+// }).catch((reject) => {
+//   console.log(reject.message);
+// }).finally(() => {
+//   console.log('request completed');
+// });
--- typescript-promises/take-a-chance.ts
+++ typescript-promises/take-a-chance.ts
@@ -2,9 +2,10 @@ export function takeAChance(name: string): Promise<string> {
   // returning promise object
   return new Promise((resolve, reject) => {
     // returning new object Promise with two parameters
+    // delay 2 seconds
     setTimeout(() => {
       // acting like a function
-      Math.random() <= 0.5 // condition if less than with random si it could be less or greater in order to get reject or resolve
+      Math.random() <= 0.5 // condition if less than with random  it could be less or greater in order to get reject or resolve like real server
         ? // if promise fulfill                           :                 else promise reject
           resolve(`Hooray! You're so lucky, ${name}!`)
         : reject(new Error(`It's just bad luck, ${name}.`));

6dbd203dacc1dd92038f0b10a6f05d63bb57e1fd
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-26 07:42:42 +0000

    adding files

--- typescript-async-await/read.ts
+++ typescript-async-await/read.ts
@@ -2,13 +2,13 @@
  * Simulates a network call by delaying for one second and returning a message.
  * If `doResolve` is truthy, it will resolve the Promise, otherwise it will reject.
  * Uses `setTimeout` to mimic a slow network connection.
- */
-// export function read(url: string, doResolve = true): Promise<string>
-// {
-//   return new Promise((resolve, reject) => {
-//     return setTimeout(() =>
-//       doResolve
-//           ? resolve(`read ${url}`)
-//           : reject(new Error(`rejected ${url}`)), 1000);
-//   });
-// }
+ */                               // we did true to solve the errors in awaits.ts where read() because it requires 2 parameters
+export function read(url: string, doResolve = true): Promise<string>
+{
+  return new Promise((resolve, reject) => {
+    return setTimeout(() =>
+      doResolve
+          ? resolve(`read ${url}`)
+          : reject(new Error(`rejected ${url}`)), 1000);
+  });
+}

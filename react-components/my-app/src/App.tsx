bf416fa91fb7784c14b134e03871278729a58eaf
NaeemK1188 <naeemalbirkdar@aol.com>
2025-02-11 18:51:16 +0000

    complete react-component

--- react-components/my-app/src/App.tsx
+++ react-components/my-app/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '../../../../../vite.svg';
+import { Header } from './Header';
+import { Image } from './Image';
+import { Caption } from './Caption';
+import { Description } from './Description';
+import { Button } from './Button';
+
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +33,12 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Header />
+      <Image />
+      <Caption />
+      <Description />
+      <Button />
     </>
   );
 }

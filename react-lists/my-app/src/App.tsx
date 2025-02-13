8fc5d4804607b8021954f9f3eaa3ffe5b630cb97
NaeemK1188 <naeemalbirkdar@aol.com>
2025-02-13 11:23:06 +0000

    complete react-lists

---
+++ react-lists/my-app/src/App.tsx
@@ -0,0 +1,44 @@
+// import { useState } from 'react'
+import './App.css';
+import { PokemonList, Pokedex } from './PokemonList';
+
+const pokedex: Pokedex[] = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' },
+];
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <PokemonList pokedex={pokedex} />
+    </>
+  );
+}
+
+export default App;
+
+/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */

ec67d0be633778cec1e1cf194861c4c11d0cc950
NaeemK1188 <naeemalbirkdar@aol.com>
2025-03-03 23:04:02 +0000

    adding comments

--- express-routes/server.ts
+++ express-routes/server.ts
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 
 // the server output or response on server-side when the client initiate request
+// parameters comes in position, so if we delete req, typescript will think res is req because it
+// came first in (res, next)
+// if we put the .use() block of code at the end, it wont get executed only when its undefined the id number
 app.use((req, res, next) => {
   // console.log('Hello, World!');
   console.log('The date is', new Date());
@@ -16,28 +19,22 @@ app.use((req, res, next) => {
 //   console.log('Express server listening on port 8080');
 // });
 
-app.get('/', (req, res, next) => {
+// we don't have to put next() because we are sending response to client
+// next() only applied in use()
+app.get('/', (req, res) => {
   // console.log('Hello, World!');
-  console.log('The date is', new Date());
-  console.log('Request method:', req.method);
-  console.log('Request path:', req.path);
+  console.log('home page');
   res.send('default page');
-
-  next();
 });
 
 // app.listen(8080, () => {
 //   console.log('Express server listening on port 8080');
 // });
 
-app.get('/notes', (req, res, next) => {
+app.get('/notes', (req, res) => {
   // console.log('Hello, World!');
-  console.log('The date is', new Date());
-  console.log('Request method:', req.method);
-  console.log('Request path:', req.path);
+  console.log('viewing notes page');
   res.send('viewing notes');
-
-  next();
 });
 
 // app.listen(8080, () => {
@@ -45,15 +42,16 @@ app.get('/notes', (req, res, next) => {
 // });
 
 // inside notes get the note with an id 42 or like viewing certain note on the web browser
-app.post('/notes/:noteId', (req, res, next) => {
+// .post("path", (takes arrow function) => {})
+app.post('/notes/:noteId', (req, res) => {
   // console.log('Hello, World!');
   const { noteId } = req.params;
-  console.log('The date is', new Date());
-  console.log('Request method:', req.method);
-  console.log('Request path:', req.path);
-  res.send(noteId); // what is in the webpage or the contents of the webpage, which will show 42
-
-  next();
+  // we don't call next on put or post http requests
+  console.log('adding new note id');
+  res.send(`got a note ${noteId}`); // what is in the webpage or the contents of the webpage, which will show 42
+  // res is sent to client
+  // or res.send(req.params.noteId) and removing line 50
+  // res.send(); ends the execution
 });
 // .listen is only applied at the bottom. only one listen for all the express http requests
 app.listen(8080, () => {
